bfe2473bb8ea5373731edcd4109a28ab2010c338
NikitaAlekseevich <tnikitaalekseevich@gmail.com>
2023-06-11 20:52:39 +0300

    Рабочий dashboard

--- fooddeliveryapp/app/Http/Controllers/OrderManager.php
+++ fooddeliveryapp/app/Http/Controllers/OrderManager.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Orders;
+use App\Models\Products;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,19 @@ class OrderManager extends Controller
 {
     function newOrders(){
         $orders = Orders::where("status", "open")->get();
+        $orders = json_decode(json_encode($orders));
         $delivery_boys = User::where("type", "delivery")->get();
+        $products = Products::get();
+        foreach($orders as $key => $order){
+            $order_item_ids = json_decode($order->items);
+            foreach($order_item_ids as $key2 => $order_item){
+                foreach($products as $product){
+                    if($order_item -> item_id == $product->id){
+                        $orders[$key]->item_details[$key2] = $product;
+                    }
+                }
+            }
+        }
         return view("dashboard", compact("orders", "delivery_boys"));
     }
 }
--- fooddeliveryapp/routes/web.php
+++ fooddeliveryapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthManager;
+use App\Http\Controllers\OrderManager;
 use App\Http\Controllers\ProductManager;
 use App\Http\Middleware\RoleAdmin;
 use Illuminate\Support\Facades\Route;
@@ -9,19 +10,17 @@ Route::get('/', function () {
     return "hi";
 });
 
-Route::get('dashboard', function(){
-    return "dashboard";
-})->name('dashboard');
+
 
 Route::get("login", [AuthManager::class, "login"])->name("login");
 Route::get("logout", [AuthManager::class, "logout"])->name("logout");
 Route::post("login", [AuthManager::class, "loginPost"])->name("login.post");
 
 Route::prefix("admin")->middleware(RoleAdmin::class)->group(function(){
-    Route::get('dashboard', function(){
-        return "dashboard";
-    })->name('dashboard');
+    Route::get('dashboard', [OrderManager::class,"newOrders"])->name('dashboard');
     Route::get('products', [ProductManager::class, "listProducts"])->name("products");
     Route::post('products', [ProductManager::class, "addProducts"])->name("product.add");
-    Route::get('products/delete', [ProductManager::class, "deleteProducts"])->name("product.delete");
+    Route::get('product/delete', [ProductManager::class, "deleteProducts"])->name("product.delete");
+    Route::post('order/assign', [OrderManager::class, "assignOrder"])->name("order.assign");
+    Route::get('order/list', [OrderManager::class, "listOrders"])->name("order.list");
 });
